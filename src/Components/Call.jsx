70b1b9f13c2a9f9a7a62ae1f6f4c7e74d4ab394b
FurdikiSherpa <118099799+Furdikisherpa@users.noreply.github.com>
2024-11-18 21:06:11 +0545

    new updates in call components

--- src/Components/Call.jsx
+++ src/Components/Call.jsx
@@ -1,12 +1,11 @@
 import { FaPhoneVolume } from "react-icons/fa6";
 import './Call.css';
 
-
 const Call = () => {
   return (
     <div className="phone-icon">
-      <FaPhoneVolume className="phone-icon-icon" /> 
-      <span className="phone-number">01-5913913</span>
+      <FaPhoneVolume className="phone-icon-icon" />
+      <a href="tel:015913913" className="phone-number">01-5913913</a>
     </div>
   );
 };
